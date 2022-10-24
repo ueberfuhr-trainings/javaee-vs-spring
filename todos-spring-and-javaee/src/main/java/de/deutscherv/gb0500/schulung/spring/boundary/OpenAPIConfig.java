651bd18cb30c936d2b291a4a4df80fcadd3b6b18
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-24 13:12:21 +0200

    Add Swagger UI

---
+++ todos-spring-and-javaee/src/main/java/de/deutscherv/gb0500/schulung/spring/boundary/OpenAPIConfig.java
@@ -0,0 +1,44 @@
+package de.deutscherv.gb0500.schulung.spring.boundary;
+
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocConfiguration;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springdoc.core.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.core.SpringDocWebMvcConfiguration;
+import org.springdoc.webmvc.ui.SwaggerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+@Import({ 
+	SpringDocConfiguration.class, 
+	SpringDocWebMvcConfiguration.class, 
+	SpringDocConfigProperties.class,
+	SwaggerConfig.class,
+	SwaggerUiOAuthProperties.class 
+})
+public class OpenAPIConfig {
+
+	@Bean
+	SwaggerUiConfigProperties swaggerUiConfig() {
+		SwaggerUiConfigProperties result = new SwaggerUiConfigProperties();
+		result.setConfigUrl("/spring/v3/api-docs/swagger-config");
+		result.setDisableSwaggerDefaultUrl(true);
+		return result;
+	}
+	
+	
+	@Bean
+	public OpenAPI api() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Todos Management Service")
+				.description("Manages todos, topics, priorities and assignees.").version("1.0"));
+	}
+
+}
