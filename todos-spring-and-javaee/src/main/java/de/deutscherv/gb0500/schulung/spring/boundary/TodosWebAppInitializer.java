fe547bb3ac72b232591a824c036d3d217b555781
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-10-24 14:10:01 +0200

    Some other stuff

--- todos-spring-and-javaee/src/main/java/de/deutscherv/gb0500/schulung/spring/boundary/TodosWebAppInitializer.java
+++ todos-spring-and-javaee/src/main/java/de/deutscherv/gb0500/schulung/spring/boundary/TodosWebAppInitializer.java
@@ -1,15 +1,11 @@
 package de.deutscherv.gb0500.schulung.spring.boundary;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import de.deutscherv.gb0500.schulung.spring.AppConfig;
