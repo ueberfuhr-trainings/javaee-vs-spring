a973dafe165c0f5830a94b4d0ad1cdb06a6e0a2c
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-11-27 13:27:41 +0100

    Include BeanValidation Method Interceptor

---
+++ todos-spring/src/main/java/de/sample/javax/todos/domain/ValidationConfig.java
@@ -0,0 +1,25 @@
+package de.sample.javax.todos.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.enterprise.inject.spi.CDI;
+import javax.validation.Validator;
+
+@Configuration
+public class ValidationConfig {
+
+    @Bean
+    Validator validator() {
+        return CDI.current()
+                .select(Validator.class)
+                .get();
+    }
+
+    @Bean
+    MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+}
